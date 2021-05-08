d47847a8aae1fe4501bae02d9945c51a10b6576e
AsifSyeed <37419466+AsifSyeed@users.noreply.github.com>
2021-05-09 03:41:50 +0600

    using bootstrap -> adding navbar

--- skill/views.py
+++ skill/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-def home (request):
+def home(request):
 	return render(request, 'index.html')
 
 def about(request):
-	return render(request, 'demo/about.html')
+	return render(request, 'about.html')
 
 def contact(request):
-	return render(request, 'demo/contact.html')
+	return render(request, 'contact.html')
