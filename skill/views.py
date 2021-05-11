50620dd2680c2955160fccccc95d5080155101fa
AsifSyeed <37419466+AsifSyeed@users.noreply.github.com>
2021-05-11 22:19:34 +0600

    showing data dynamically in frontend

--- skill/views.py
+++ skill/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 
 def home(request):
-	return render(request, 'index.html')
+
+	title = 'Hello everyone this is nemes1s'
+	descript = 'Developer/ Music Composer/ Gamer'
+
+
+	return render(request, 'index.html', {'title': title, 'description': descript})
 
 def about(request):
-	return render(request, 'about.html')
+
+	title = 'About Me'
+	descript = 'I am a developer working with a mindset to grow my skills. Besides, I work as music composer/ guitarist/ bassist. I play games in my pastimes.'
+
+	context = {
+		'title': title,
+		'aboutDescription': descript,
+	}
+	return render(request, 'about.html', context)
 
 def contact(request):
 	return render(request, 'contact.html')
