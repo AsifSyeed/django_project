75da48b257b1b94fc05546b57723abc3cc7bc103
AsifSyeed <37419466+AsifSyeed@users.noreply.github.com>
2021-05-11 21:12:46 +0600

    using models

--- skill/models.py
+++ skill/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class skill(models.Model):
+	image = models.ImageField(upload_to='skillImage/')
+	title = models.CharField(max_length=50, blank=False)
+	descipt = models.TextField(max_length=500, blank=True)
+	dateTime = models.DateTimeField(auto_now_add=True)
+
